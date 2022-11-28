18922bf85e53fcfdedd1d5a5f22a6d3954d01369
Pavel Mazin <iaa.aka@yandex.ru>
2022-11-28 12:28:34 +0000

    add mans

--- test/test.R
+++ test/test.R
@@ -11,7 +11,7 @@ devtools::install_github("iaaka/visutils")
 
 devtools::install_local()
 # git add -u
-# git commit -m "improve dotPlot"
+# git commit -m "fix myLoadH5AD_Spatials"
 # git push -u origin main
 ?dotPlot
 
